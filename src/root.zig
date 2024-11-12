9680c7fff9a8f68a3d270b3133f69035cfa69254
pegasus.cadence@gmail.com <pegasus.cadence@gmail.com>
2024-11-11 17:54:37 -0800

    Empty range for inclusive should iterate just once, invalid range will do nothing

--- src/root.zig
+++ src/root.zig
@@ -27,15 +27,17 @@ pub fn WhileRange(comptime T: type) type {
         const Self = @This();
 
         pub fn init(start: T, end: T, inclusive: bool, step: SignedT) !Self {
+            //std.debug.print("Original start: {d} end: {d} step: {d} inclusive:{}\n", .{ start, end, step, inclusive });
             // Validate step isn't zero
             if (0 == step) return error.ZeroStep;
+            var negative_range = false;
             var range_size: T = 0;
             var steps: T = 0; //@divFloor(@abs(end - start), @abs(step));
             var end_: T = end;
-            if (start <= end) {
+            if (start < end) {
                 if (0 > step) {
-                    // empty range
-                    end_ = start;
+                    // negative range
+                    negative_range = true;
                 } else {
                     range_size = (end - start);
                     if (comptime @typeInfo(T) == .Int) {
@@ -44,10 +46,9 @@ pub fn WhileRange(comptime T: type) type {
                         steps = @divFloor(range_size, step);
                     }
                 }
-            } else {
+            } else if (start > end) {
                 if (0 < step) {
-                    // empty range
-                    end_ = start;
+                    negative_range = true;
                 } else {
                     range_size = (start - end);
                     if (comptime @typeInfo(T) == .Int) {
@@ -57,28 +58,44 @@ pub fn WhileRange(comptime T: type) type {
                     }
                 }
             }
-            //std.debug.print("start: {d} end: {d} step: {d} range_size: {d} end_: {d}\n", .{ start, end, step, range_size, end_ });
-
-            if (comptime @typeInfo(T) == .Int) {
-                end_ = @as(T, @intCast(@as(SignedT, @intCast(start)) + @as(SignedT, @intCast(steps)) * step));
+            //  else {
+            //     // exclusive empty range
+            //     // inclusive one iteration range
+            //     // do nothing
+            //     steps = 0;
+            // }
+            if (negative_range) {
+                end_ = start;
             } else {
-                end_ = start + steps * step;
-            }
+                //std.debug.print("After range_size, start: {d} end: {d} step: {d} range_size: {d} end_: {d}\n", .{ start, end, step, range_size, end_ });
 
-            var not_on_step = end_ != end;
-            if (comptime @typeInfo(T) == .Float) {
-                not_on_step = @abs(end_ - end) > @abs(step / 10);
-                //std.debug.print("end_: {d} end: {d} step: {d} not_on_step: {}\n", .{ end_, end, step, not_on_step });
-            }
-            if ((0 < range_size) and (inclusive or not_on_step)) {
-                if (comptime @typeInfo(T) == .Int) {
-                    end_ = @as(T, @intCast(@as(SignedT, @intCast(end_)) + step));
-                } else {
-                    end_ = end_ + step;
+                if (0 != range_size) {
+                    if (comptime @typeInfo(T) == .Int) {
+                        // May overflow, need carefully test and cast to extended signedT ( u8 -> i16 )
+                        end_ = @as(T, @intCast(@as(SignedT, @intCast(start)) + @as(SignedT, @intCast(steps)) * step));
+                    } else {
+                        end_ = start + steps * step;
+                    }
+                }
+                //std.debug.print("After start + steps * step, start: {d} end: {d} step: {d} range_size: {d} end_: {d}\n", .{ start, end, step, range_size, end_ });
+
+                var not_on_step = end_ != end;
+                if (comptime @typeInfo(T) == .Float) {
+                    not_on_step = @abs(end_ - end) > @abs(step / 10);
+                    //std.debug.print("end_: {d} end: {d} step: {d} not_on_step: {}\n", .{ end_, end, step, not_on_step });
+                }
+
+                if (inclusive or (0 < range_size and not_on_step)) {
+                    if (comptime @typeInfo(T) == .Int) {
+                        end_ = @as(T, @bitCast(@as(SignedT, @bitCast(end_)) + step));
+                    } else {
+                        end_ = end_ + step;
+                    }
+                    //std.debug.print("Adjust for inclusive or not_on_step, start: {d} end: {d} step: {d} range_size: {d} end_: {d}\n", .{ start, end, step, range_size, end_ });
                 }
             }
 
-            //std.debug.print("start: {d} end: {d} step: {d} range_size: {d} end_: {d}\n", .{ start, end, step, range_size, end_ });
+            //std.debug.print("Finally, start: {d} end: {d} step: {d} range_size: {d} end_: {d}\n", .{ start, end, step, range_size, end_ });
 
             return Self{
                 .start = start,
@@ -105,17 +122,18 @@ pub fn WhileRange(comptime T: type) type {
 
                 if (!valid) return null;
             } else {
-                const valid = if (self.step > 0)
-                    self.curr < self.end
-                else
-                    self.curr > self.end;
+                // const valid = if (self.step > 0)
+                //     self.curr < self.end
+                // else
+                //     self.curr > self.end;
+                const valid = self.curr != self.end;
 
                 if (!valid) return null;
             }
 
             const value = self.curr;
             if (comptime @typeInfo(T) == .Int) {
-                self.curr = @as(T, @intCast(@as(SignedT, @intCast(self.curr)) + self.step));
+                self.curr = @as(T, @bitCast(@as(SignedT, @bitCast(self.curr)) + self.step));
             } else {
                 self.curr = self.curr + self.step;
             }
@@ -197,6 +215,13 @@ test "test_lib_main" {
     std.debug.print("Running tests in src/root.zig \"main\"\n", .{});
 
     try lib_main();
+}
+
+test "signed/unsigned int cast" {
+    const signed: i8 = -1;
+    const unsigned: u8 = @as(u8, @bitCast(signed)); // Cast -1 i8 to 255 u8
+    //std.debug.print("unsigned: {}\n", .{unsigned}); // Prints: 255
+    try std.testing.expectEqual(unsigned, 255);
 }
 
 test "std u8 start stop exclusive step is 1" {
@@ -308,6 +333,74 @@ test "std u8 start stop ex/inclusive empty range" {
     try std.testing.expectEqual(s_i, 0);
 }
 
+test "std u8 forward start equals stop ex empty range in one iteration range" {
+    // Integer range (forward)
+    var s_e: u16 = 0;
+    var range_e = try WhileRange(u8).init(1, 1, false, 1);
+    while (range_e.next()) |value| {
+        s_e += value;
+    }
+    try std.testing.expectEqual(s_e, 0);
+
+    var s_i: u16 = 0;
+    var range_i = try WhileRange(u8).init(1, 1, true, 1);
+    while (range_i.next()) |value| {
+        s_i += value;
+    }
+    try std.testing.expectEqual(s_i, 1);
+}
+
+test "std u8 backward start equals stop ex empty range in one iteration range" {
+    // Integer range (forward)
+    var s_e: u16 = 0;
+    var range_e = try WhileRange(u8).init(1, 1, false, -1);
+    while (range_e.next()) |value| {
+        s_e += value;
+    }
+    try std.testing.expectEqual(s_e, 0);
+
+    var s_i: u16 = 0;
+    var range_i = try WhileRange(u8).init(1, 1, true, -1);
+    while (range_i.next()) |value| {
+        s_i += value;
+    }
+    try std.testing.expectEqual(s_i, 1);
+}
+
+test "std u8 forward start equals stop ex empty range in one iteration range step over 1" {
+    // Integer range (forward)
+    var s_e: u16 = 0;
+    var range_e = try WhileRange(u8).init(1, 1, false, 2);
+    while (range_e.next()) |value| {
+        s_e += value;
+    }
+    try std.testing.expectEqual(s_e, 0);
+
+    var s_i: u16 = 0;
+    var range_i = try WhileRange(u8).init(1, 1, true, 2);
+    while (range_i.next()) |value| {
+        s_i += value;
+    }
+    try std.testing.expectEqual(s_i, 1);
+}
+
+test "std u8 backward start equals stop ex empty range in one iteration range step over 1" {
+    // Integer range (forward)
+    var s_e: u16 = 0;
+    var range_e = try WhileRange(u8).init(1, 1, false, -2);
+    while (range_e.next()) |value| {
+        s_e += value;
+    }
+    try std.testing.expectEqual(s_e, 0);
+
+    var s_i: u16 = 0;
+    var range_i = try WhileRange(u8).init(1, 1, true, -2);
+    while (range_i.next()) |value| {
+        s_i += value;
+    }
+    try std.testing.expectEqual(s_i, 1);
+}
+
 test "std i8 start stop ex/inclusive empty range" {
     // Integer range (forward)
     var s_e: i16 = 0;
@@ -361,14 +454,14 @@ test "std f32 start stop backward exclusive step is -0.2" {
 }
 
 test "indexed while range" {
-    var index :usize = 0;
+    var index: usize = 0;
     var range = try IndexedWhileRange(u8).init(10, 1, true, -1);
     std.debug.print("Indexed backward u8 while range [10, 1]:\n", .{});
     std.debug.print("(Index:Value)", .{});
     while (range.next()) |element| {
         try std.testing.expectEqual(index, element.index);
         index += 1;
-        std.debug.print("({}:{})\n", .{element.index, element.value});
+        std.debug.print("({}:{})\n", .{ element.index, element.value });
     }
     std.debug.print("\n", .{});
 }
